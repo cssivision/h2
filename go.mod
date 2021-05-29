60e1ece1cf108f6e22d7898b44cae09e48260676
cssivision <cssivision@gmail.com>
2021-05-29 15:48:44 +0800

    initial client handshake

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cssivision/h2s
 
 go 1.15
+
+require golang.org/x/net v0.0.0-20210525063256-abc453219eb5
--- h2s.go
+++ h2s.go
@@ -1,1 +1,41 @@
 package h2s
+
+import (
+	"io"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/hpack"
+)
+
+const (
+	defaultMaxHeaderListSize = uint32(16 << 20)
+	defaultMaxFramSize       = uint32(16384)
+	defaultHeaderTableSize   = uint32(4096)
+	defaultWindowSize        = 65535
+)
+
+// ConnectOptions options for http2 connect with the server.
+type ConnectOptions struct {
+	InitialWindowSize         uint32
+	InitialConnWindowSize     uint32
+	MaxFrameSize              uint32
+	MaxHeaderListSize         *uint32
+	MaxConcurrentStreams      uint32
+	InitialMaxSendStreams     uint32
+	MaxConcurrentResetStreams uint32
+	EnablePush                bool
+}
+
+type framer struct {
+	fr *http2.Framer
+}
+
+func newFramer(conn io.ReadWriteCloser, maxFramSize, maxHeaderListSize uint32) *framer {
+	f := &framer{
+		fr: http2.NewFramer(conn, conn),
+	}
+	f.fr.SetMaxReadFrameSize(maxFramSize)
+	f.fr.MaxHeaderListSize = maxHeaderListSize
+	f.fr.ReadMetaHeaders = hpack.NewDecoder(defaultHeaderTableSize, nil)
+	return f
+}
